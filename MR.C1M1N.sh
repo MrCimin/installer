3e909ec96a2d354782d0c3a2fda17b909543e638
MrCimin <65604994+MrCimin@users.noreply.github.com>
2020-05-20 19:04:57 +0700

    Add files via upload

---
+++ MR.C1M1N.sh
@@ -0,0 +1,445 @@
+#!/bin/sh
+#Tools Installer
+#Monggoh Re-Code Aja Gpp
+
+clear
+biru='\033[36;1m'
+kuning='\33[33;1m'
+merah='\33[31;1m'
+putih='\33[37;1m'
+hijau='\33[32;1m'
+echo $biru "
+ ______________[Selamat Datang]
+/
+\______________[Apa Kabar Kalian]
+/
+\______________[Semoga Baik Baik Saja]
+/
+\______________[Tunggu Bentar]
+/
+\______________[Masih Di Install]"
+sleep 3
+echo $kuning"IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"
+sleep 5
+apt update && apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install ruby -y
+gem install lolcat 
+clear
+echo $merah"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo $merah":::::#:::::::::::#::::#######:::::::::::########::::#:::#:::::::::::#:::#:::#::::::::::#::::::"
+echo $merah":::::#:#:::::::#:#::::#:::::#:::::::::::#:::::::::::#:::#:#:::::::#:#:::#:::#:#::::::::#::::::"
+echo $merah":::::#::#:::::#::#::::#:::::#:::::::::::#:::::::::::#:::#::#:::::#::#:::#:::#::#:::::::#::::::"
+echo $merah":::::#:::#:::#:::#::::#:::::#:::::::::::#:::::::::::#:::#:::#:::#:::#:::#:::#:::#::::::#::::::"
+echo $merah":::::#::::#:#::::#::::#######:::::::::::#:::::::::::#:::#::::#:#::::#:::#:::#::::#:::::#::::::"
+echo $putih":::::#:::::#:::::#::::#:#:::::::::::::::#:::::::::::#:::#:::::#:::::#:::#:::#:::::#::::#::::::"
+echo $putih":::::#:::::::::::#::::#::#::::::::::::::#:::::::::::#:::#:::::::::::#:::#:::#::::::#:::#::::::"
+echo $putih":::::#:::::::::::#::::#:::#:::::::::::::#:::::::::::#:::#:::::::::::#:::#:::#:::::::#::#::::::"
+echo $putih":::::#:::::::::::#::::#::::#:::::###::::#:::::::::::#:::#:::::::::::#:::#:::#::::::::#:#::::::"
+echo $putih":::::#:::::::::::#::::#:::::#::::###::::########::::#:::#:::::::::::#:::#:::#::::::::::#::::::"
+echo $putih"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+sleep 10
+echo ""
+echo ""
+echo IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII | lolcat -a -d 5
+read -p "Masukan Nama Anda >>" d
+echo IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII | lolcat -a -d 5
+figlet -f slant "Hallo , Welcome" | lolcat -a -d 3
+figlet -f slant "To My Program" | lolcat -a -d 3
+figlet -f slant "$d" | lolcat -a -d 3
+sleep 5
+clear
+cowsay -f eyes 'Hallo' | lolcat
+sleep 2
+clear
+cowsay -f eyes 'Jika Tidak Punya Lisensi Chat No 0895606026148' | lolcat
+p="Indr+62"
+
+while [ "$q" != "$p" ]
+do
+read -p "Masukan Lisensi Anda :" q
+done
+if [ $q = "Indr+62" ]
+then
+    clear
+    echo "Selamat Datang Di Program Saya" | lolcat
+    echo "Silakan Menikmati Script Yang Saya Sediakan" | lolcat
+    sleep 3
+    echo ""
+    echo ""
+    echo ""
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" | lolcat
+    echo "Last Update : 16-11-2019" | lolcat
+    echo "Version     : 1.6 Alpha" | lolcat
+    echo "Note        : Maaf Jarang Update Script Karna 
+              Hape Saya Sedang Ada Masalah" | lolcat
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" | lolcat
+    echo "(1).Perbaikan Bug Tampilan Pada Script Ini" | lolcat
+    echo "(2).Optimalisasi Tampilan Script" | lolcat
+    echo "(3).Penambahan Beberapa Script Baru" | lolcat
+    echo "(4).Perbaikan Script Dark Fb" | lolcat
+    echo "(5).  ----|||----- " | lolcat
+    echo "× Jika Ada Bug Lapor Ke Saya : +62895606026148 ×" | lolcat
+    echo "×Atau Bisa Juga Melalui Facebook Saya : Silakan Copy Link Di Bawah Ini" | lolcat
+    echo "Ini Linknya : https://www.facebook.com/muammar.khadafi.589100" | lolcat
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" | lolcat
+    sleep 10
+    clear
+    screenfetch -A Debian
+    echo $hijau"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    echo "Author    : MR.C1M1N" | lolcat
+    echo "Phone   : +62895606026148" | lolcat
+    echo "Pengguna  : $d" | lolcat
+    echo $hijau"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    echo ""
+    ulang="."
+    while [ $ulang = "." ]
+    do
+    echo $merah "                |______________[Menu Script]__________________|
+                 |
+                 |
+                 |----(1).Install Bahan
+                 |
+                 |----(2).Install Lacak Lokasi
+                 |
+                 |----(3).Install Ko-Dork
+                 |
+                 |----(4).Install Ubuntu
+                 |
+                 |----(5).Install Dark FB(Dalam Perbaikan Jadi Jangan Di Pilih)
+                 |
+                 |----(6).Install Sql Map
+                 |
+                 |----(7).Install Ddos Botnet
+                 |
+                 |----(8).Install Script Devace Creator
+                 |
+                 |----(9).Install Tombol Termux"
+    echo $kuning "                |
+                 |----(10).Install Sadap Galeri
+                 |
+                 |----(11).Install Tools Serbaguna
+                 |
+                 |----(12).Install Hack Cctv
+                 |
+                 |----(13).Install Santet Online
+                 |
+                 |----(14).Install Hack Wifi(ROOT)
+                 |
+                 |----(15).Install Red_Hawk
+                 |
+                 |----(16).Penjelasan SCRIPT
+                 |
+                 |----(17).Install OSIF (Open Source Information Facebook)
+                 |
+                 |----(18).Install Vbug
+                 |
+                 |----(19).Install Metasploit
+                 |
+                 |----(20).Install Warna Termux"
+    echo $hijau "                |______________________________________________|
+                 |
+                 |----Coming Soon
+                 |
+                 |----Coming Soon
+                 |
+                 |----Coming Soon
+                 |
+                 |----(99).Exit
+                 |______________________________________________|"
+    echo ""
+    echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX | lolcat -a -d 3
+    read -p "Masukan Pilihan Anda $d >>" g;
+    echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX | lolcat -a -d 3
+    if [ $g = "1" ]
+    then
+        echo "Installing Biar Tools Lancar Semua..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install git -y
+        apt install python -y
+        apt install python2 -y
+        apt install curl -y
+        apt install wget -y
+        apt install openssh -y
+        apt install mc -y
+        apt install php -y
+        apt install openssh -y
+        apt install toilet -y
+        apt install figlet -y
+        apt install screenfetch -y
+        apt install ruby -y
+        apt install neofetch -y
+        apt install nano -y
+        gem install lolcat
+        pip install requests
+        pip install mechanize
+        pip install bs4
+        echo "Sudah Semua Tod :)" | lolcat
+        sleep 2
+        clear
+        echo $ulang
+    elif [ $g = "2" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install git -y
+        git clone https://github.com/thelinuxchoice/locator
+        cd locator
+        bash locator.sh
+        exit
+    elif [ $g = "3" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install python -y
+        apt install python2 -y
+        apt install git -y
+        git clone https://github.com/CiKu370/ko-dork
+        cd ko-dork
+        python2 dork.py
+        exit
+    elif [ $g = "4" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install git -y
+        apt install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+        ./start-ubuntu.sh
+        exit
+    elif [ $g = "5" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install python -y
+        apt install python2 -y
+        apt install git
+        git clone https://github.com/muhalvin/darkfb7
+        cd darkfb7
+        python2 dark.py
+        exit
+    elif [ $g = "6" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install git -y
+        apt install python -y
+        apt install python2 -y
+        git clone https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+        cd sqlmap-dev
+        python2 sqlmap.py --help
+        exit
+    elif [ $g = "7" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install git -y
+        apt install python -y
+        apt install python2 -y
+        git clone https://github.com/nishinosukasa/Bomb-Botnet
+        cd Bomb-Botnet
+        python2 Bomb-Botnet.py
+        exit
+    elif [ $g = "8" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install python -y
+        apt install python2 -y
+        apt install git -y
+        git clone https://github.com/4L13199/LITESCRIPT
+        cd LITESCRIPT
+        python2 LITESCRIPT.py
+        exit
+    elif [ $g = "9" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install python -y
+        apt install python2 -y
+        git clone https://github.com/karjok/terkey
+        cd terkey
+        python2 terkey.py
+        exit
+    elif [ $g = "10" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install git -y
+        apt install openssh -y
+        apt install mc -y
+        git clone https://github.com/thelinuxchoice/saycheese
+        cd saycheese
+        bash saycheese.sh
+        exit
+    elif [ $g = "11" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install python -y
+        apt install python2 -y
+        apt install git -y
+        pip install requests
+        git clone https://github.com/Rusmana-ID/rus
+        cd rus
+        bash requests.sh
+        echo "USERNAME : Garuda         PW : 45" | lolcat
+        echo "Jika Salah Username & Passwordnya Maka Anda Harus Download Sendiri Nanti Di Dalam Script Anda otomatis Akan Di Bawa Ke Browser Untuk Download Username & Password Terbarunya :)" | lolcat
+        sleep 5
+        exit
+    elif [ $g = "12" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $Home
+        apt update && apt upgrade -y
+        apt install python -y
+        apt install python2 -y
+        apt install git -y
+        pip install requests
+        git clone https://github com/kancotdiq/ipcs
+        cd ipcs
+        python2 scan.py
+        exit
+    elif [ $g = "13" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install python -y
+        apt install python2 -y
+        git clone https://github.com/Gameye98/santet-online
+        cd santet-online
+        python2 santet.py
+        exit
+    elif [ $g = "14" ]
+    then
+        echo "Ingat Tools Ini Membutuhkan Akses ROOT" | lolcat
+        sleep 3
+        echo "Atau Bisa Menggunakan Chroot Ubuntu" | lolcat
+        sleep 3
+        echo "Jika Tidak Tahu Hentikan Program Ini Dan Ulang Script Lalu Pilih No.4" | lolcat
+        sleep 3
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install git -y
+        git clone https://github.com/esc0rtd3w/wifi-hacker
+        cd wifi-hacker
+        chmod +x wifi-hacker.sh
+        ./wifi-hacker.sh
+        exit
+    elif [ $g = "15" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade -y
+        apt install php -y
+        apt install git -y
+        git clone https://github.com/Tuhinshubhra/RED_HAWK
+        cd RED_HAWK
+        php rhawk.php
+        exit
+    elif [ $g = "16" ]
+    then
+        sh Penjelasan.sh
+    elif [ $g = "17" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 2
+        cd $HOME
+        apt update && apt upgrade
+        apt install python
+        apt install python2
+        apt install git
+        git clone https://github.com/CiKu370/OSIF
+        cd OSIF
+        pip install -r requirements.txt
+        python2 osif.py
+        exit
+    elif [ $g = "18" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt update && apt upgrade
+        apt install python
+        apt install python2
+        apt install git
+        git clone https://github.com/Junior60/vbug
+        cd vbug
+        python2 vbug.py
+        exit
+    elif [ $g = "19" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 2
+        echo "Mungkin Penginstalan Ini Membutuhkan Waktu Yang Cukup Lama" | lolcat
+        sleep 3
+        cd $HOME
+        pkg install unstable-repo
+        pkg install metasploit
+        exit
+        echo "Untuk Menjalankannya Ketik (msfconsole)" | lolcat
+        sleep 3
+    elif [ $g = "20" ]
+    then
+        echo "Installing..." | lolcat
+        sleep 3
+        cd $HOME
+        apt install curl
+        apt install wget
+        sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+        exit
+    elif [ $g = "99" ]
+    then
+        echo "Please Wait..." | lolcat
+        sleep 10
+        cowsay -f eyes 'Good Bye' | lolcat
+        sleep 5
+        clear
+        sleep 2
+        cd $HOME
+        sleep 2
+        exit
+    else
+        cowsay -f eyes 'Input Yang Kamu Masukan Salah' | lolcat
+        sleep 2
+        clear
+        echo $ulang
+    fi
+    done
+else
+    echo "Jika Tidak Punya Lisensi Hub Saya 0895606026148" | lolcat
+    cowsay -f eyes 'Good Bye' | lolcat
+    sleep 9
+    clear
+fi
+
